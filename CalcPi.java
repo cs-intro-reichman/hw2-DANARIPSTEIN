a5857703669c3e6417a476d1ed5c78cf62f1dbe3
DANARIPSTEIN <dana.ripstein@post.runi.ac.il>
2024-11-13 18:35:28 +0200

    test2

--- CalcPi.java
+++ CalcPi.java
@@ -1,13 +1,19 @@
 // Computes an approximation of PI.
 public class CalcPi {
-	public static void main(String [] args) { 
+	public static void main(String[] args) {
 		int N = Integer.parseInt(args[0]);
-		double approximated;
-		int i = 0;
-		while(i<N){
-
-			i++;
+		double approximated = 0.0;
+		double odd = 1.0;
+		for (int i = 0; i < N; i++) {
+			if (i % 2 == 0) {
+				approximated = approximated + Math.pow((odd), (-1));
+			} else {
+				approximated = approximated - Math.pow((odd), (-1));
+			}
+			odd = odd + 2.0;
 		}
-		System.out.println("pi according to Java: 3.141592653589793");
-		System.out.println("pi, approximated: " + (double)approximated);
+		approximated = (approximated) * 4.0;
+		System.out.println("pi according to Java: " + Math.PI);
+		System.out.println("pi, approximated: " + (double) approximated);
+	}
 }
